f9048116ae88ebab9b950315610a6ad878233200
zhangwj0520 <zhangwj0520@users.noreply.github.com>
2018-09-25 16:12:02 +0800

    update readme

--- README.md
+++ README.md
@@ -1,19 +1,16 @@
-https://zhangwj0520.github.io/pizza-app/dist/index.html
-
 # pizza-app
 
-## Project setup
-```
+## 演示地址
+[pizza-app](https://node-app-0923.herokuapp.com/)
+ 已经注册测试账号：test1@qq.com/test1
+
+## 说明
+使用vue-cli3.0构建项目,主要使用vuex管理各个组件件的数据及状态,数据存储使用的是野狗云,数据请求使用的axios,实现的一个pizza点餐系统
+
+# Usage
+git https://github.com/zhangwj0520/node-app
+cd node-app
 npm install
-```
-
-### Compiles and hot-reloads for development
-```
 npm run serve
-```
-
-### Compiles and minifies for production
-```
 npm run build
 ```
-### 路由/路由嵌套
--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,52 @@
-// vue.config.js
+let path = require("path");
 module.exports = {
-    // 选项...
-    baseUrl: process.env.NODE_ENV === 'production'
-    ? './'
-    : '/'
-   
-  }
-     
+    baseUrl: '/',
+    // 输出文件目录
+    outputDir: 'dist',
+    assetsDir:"static",
+    lintOnSave: true,
+    pages: {
+        index: {
+            // entry for the page
+            entry: 'src/main.js',
+            // the source template
+            template: 'public/index.html',
+            // output as dist/index.html
+            filename: 'index.html',
+        }
+    },
+    css: {
+        // 是否使用css分离插件 ExtractTextPlugin
+        extract: true,
+        // 开启 CSS source maps?
+        sourceMap: false,
+        // css预设器配置项
+        loaderOptions: {},
+        // 启用 CSS modules for all css / pre-processor files.
+        modules: false
+    },
+    devServer: {
+        open:true,
+        host: '127.0.0.1',
+        port: 8080,
+        https: false,
+        hotOnly: false,
+        //proxy: 'http://localhost:8180' ,// 配置跨域处理,只有一个代理
+       // proxy: {
+       //      '/login': {
+       //          target: 'http://localhost:8180',
+       //          ws: true,
+       //          changeOrigin: true
+       //      },
+       //      '/foo': {
+       //          target: '<other_url>'
+       //      }
+       //  },  // 配置多个代理
+        before: app => {
+        }
+    },
+    // 第三方插件配置
+    pluginOptions: {
+        // ...
+    }
+}
